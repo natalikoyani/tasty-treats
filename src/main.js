8bc9d0f8360696fc466daa03cf9c0124df410979
natalikoyani <natalikagami@gmail.com>
2023-09-14 22:30:46 +0200

    test 3

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
-import './js/api'
-import './js/markup-rendering'
+import axios from "axios";
+import Swiper from 'swiper';
+import 'swiper/css';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select'
+// import of debounce???
+
+import './js/header'
+import './js/hero'
+import './js/categories'
+import './js/search-filters'
+import './js/cards-gallery'
+import './js/popular-recipes'
+import './js/favorites-content'
+import './js/recipe-modal'
+import './js/hamburger-menu'
