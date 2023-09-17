c60fab3894ebf357bcc745cb947647e49fa8e8f0
natalikoyani <natalikagami@gmail.com>
2023-09-17 22:28:24 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,11 @@ import './js/popular-recipes.js'
 import { getRecipes } from './js/api.js';
 import { renderRecipes } from './js/render-recipe-card.js';
 
-const cardGallery = document.querySelector('.cards-gallery');
+async function renderGallery() {
+    const cardGallery = document.querySelector('.cards-gallery');
 
-const recipes = await getRecipes({});
-cardGallery.innerHTML = await renderRecipes(recipes)
+    const recipes = await getRecipes({});
+    cardGallery.innerHTML = await renderRecipes(recipes)
+}
+
+await renderGallery();
