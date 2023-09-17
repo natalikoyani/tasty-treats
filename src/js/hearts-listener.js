c63f6120a95e8921b87e30eb33f3bcc08e2c3800
Pe4enyffka <serhyd.89@gmail.com>
2023-09-17 23:49:43 +0300

    final

--- src/js/hearts-listener.js
+++ src/js/hearts-listener.js
@@ -1,29 +1,31 @@
-const cardHearts = document.querySelectorAll('.recipe-card .heart-svg');
+export const addHeartsEventListener = () => {
 
-const addToFavorities = (event) => {
-    let dataset = event.currentTarget.dataset
-
-    // get + check localStorage
-    let currentLocalStorage = localStorage.getItem('favoriteRecipes')
-    let recipeList = currentLocalStorage ? JSON.parse(currentLocalStorage) : []
-    recipeList = Array.isArray(recipeList) ? recipeList : []
-
-    // check if recipe already in Favorites
-    let recipeId = dataset?._id
-    let indexOfRecipe = recipeList.findIndex(recipe => recipe._id === recipeId)
-
-    event.currentTarget.classList.toggle("heart-filled");
-
-    if (indexOfRecipe < 0) {
-        // add recipe to list
-        recipeList.push(dataset)
-        localStorage.setItem('favoriteRecipes', JSON.stringify(recipeList))
-    } else { 
-        // delete recipe from list
-        recipeList.splice(indexOfRecipe, 1)
-        localStorage.setItem('favoriteRecipes', JSON.stringify(recipeList))
+    const cardHearts = document.querySelectorAll('.recipe-card .heart-svg');
+    
+    const addToFavorities = (event) => {
+        let dataset = event.currentTarget.dataset
+        
+        // get + check localStorage
+        let currentLocalStorage = localStorage.getItem('favoriteRecipes')
+        let recipeList = currentLocalStorage ? JSON.parse(currentLocalStorage) : []
+        recipeList = Array.isArray(recipeList) ? recipeList : []
+        
+        // check if recipe already in Favorites
+        let recipeId = dataset?._id
+        let indexOfRecipe = recipeList.findIndex(recipe => recipe._id === recipeId)
+        
+        event.currentTarget.classList.toggle("heart-filled");
+        
+        if (indexOfRecipe < 0) {
+            // add recipe to list
+            recipeList.push(dataset)
+            localStorage.setItem('favoriteRecipes', JSON.stringify(recipeList))
+        } else { 
+            // delete recipe from list
+            recipeList.splice(indexOfRecipe, 1)
+            localStorage.setItem('favoriteRecipes', JSON.stringify(recipeList))
+        }
     }
-
+    
+    cardHearts.forEach(c => c.addEventListener('click' , addToFavorities) )
 }
-
-cardHearts.forEach(c => c.addEventListener('click' , addToFavorities) )
