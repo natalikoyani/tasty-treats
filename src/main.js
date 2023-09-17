47f9cb7da16dd2c3dd50dd4d714f6cff9c3b37bb
natalikoyani <natalikagami@gmail.com>
2023-09-17 23:05:47 +0200

    added gallery-rendering

--- src/main.js
+++ src/main.js
@@ -5,17 +5,10 @@ import { Notify } from 'notiflix';
 import SlimSelect from 'slim-select';
 import debounce from 'lodash.debounce';
 
+import { renderGallery } from './js/render-card-gallery.js';
+
 import './js/hero.js';
 import './js/fillCategories';
 import './js/popular-recipes.js'
-import { getRecipes } from './js/api.js';
-import { renderRecipes } from './js/render-recipe-card.js';
-
-async function renderGallery() {
-    const cardGallery = document.querySelector('.cards-gallery');
-
-    const recipes = await getRecipes({});
-    cardGallery.innerHTML = await renderRecipes(recipes)
-}
 
 renderGallery();
