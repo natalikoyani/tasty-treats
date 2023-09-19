311cf22a95f4950e47d85f1c045b4659daa207b9
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-09-19 16:46:07 +0300

    open modal

--- src/js/open-recipe-modal.js
+++ src/js/open-recipe-modal.js
@@ -4,32 +4,26 @@ import { renderModalRecipe } from './recipe-modal';
 const modalrecipe = document.querySelector('.js-create-modal');
 const modalConteiner = document.querySelector('.js-modal-recipe');
 
-
-export function addRecipeButton(classname){
+export function addRecipeButton(classname) {
   let btns = document.querySelectorAll(classname);
   btns.forEach(btn => {
-    btn.addEventListener('click', async (event)=> { 
+    btn.addEventListener('click', async event => {
       let recipeId = event.target.dataset._id;
       let objRecipe = await getRecipeById(recipeId);
       let renderRecipe = renderModalRecipe(objRecipe);
       modalrecipe.innerHTML = renderRecipe;
-      modalConteiner.classList.remove("visually-hidden");
-   
-     }
-     )
-  }) 
+      modalConteiner.classList.remove('visually-hidden');
+    });
+  });
 }
 
-
-
 const refs = {
-    
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.add("visually-hidden");
-  }
+function toggleModal() {
+  refs.modal.classList.add('visually-hidden');
+}
