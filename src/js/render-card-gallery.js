88a1291db6d90f30a456c477cf2f30622ab55d48
InnaKara <innatvinna@gmail.com>
2023-09-19 10:00:30 +0200

    correct

--- src/js/render-card-gallery.js
+++ src/js/render-card-gallery.js
@@ -2,8 +2,10 @@ import { getRecipes } from './api';
 import { renderRecipes } from './render-recipe-card';
 
 const cardGallery = document.querySelector('.cards-gallery');
+const allCategoriesBtn = document.querySelector('.all-categories-button');
 
 export async function renderGallery() {
-    const recipes = await getRecipes({});
-    cardGallery.innerHTML = await renderRecipes(recipes)
+  const recipes = await getRecipes({});
+  cardGallery.innerHTML = await renderRecipes(recipes);
+  allCategoriesBtn.classList.add('active-category');
 }
