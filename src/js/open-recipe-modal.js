7d7ce18f5481b6e881419df68ad0b972a4120665
Pe4enyffka <serhyd.89@gmail.com>
2023-09-20 02:23:22 +0300

    final edit

--- src/js/open-recipe-modal.js
+++ src/js/open-recipe-modal.js
@@ -4,15 +4,26 @@ import { renderModalRecipe } from './recipe-modal';
 const modalrecipe = document.querySelector('.js-create-modal');
 const modalConteiner = document.querySelector('.js-modal-recipe');
 
-export function addRecipeButton(classname) {
-  let btns = document.querySelectorAll(classname);
-  btns.forEach(btn => {
-    btn.addEventListener('click', async event => {
-      let recipeId = event.target.dataset._id;
-      openModelById(recipeId);
-    });
-  });
+export async function addRecipeButton(event) {
+  let recipeId = event.target.dataset._id;
+  let objRecipe = await getRecipeById(recipeId);
+  let renderRecipe = renderModalRecipe(objRecipe);
+  modalrecipe.innerHTML = renderRecipe;
+  modalConteiner.classList.remove('visually-hidden');
 }
+
+// export function addRecipeButton(classname) {
+//   let btns = document.querySelectorAll(classname);
+//   btns.forEach(btn => {
+//     btn.addEventListener('click', async event => {
+//       let recipeId = event.target.dataset._id;
+//       let objRecipe = await getRecipeById(recipeId);
+//       let renderRecipe = renderModalRecipe(objRecipe);
+//       modalrecipe.innerHTML = renderRecipe;
+//       modalConteiner.classList.remove('visually-hidden');
+//     });
+//   });
+// }
 
 const refs = {
   closeModalBtn: document.querySelector('[data-modal-close]'),
--- src/js/render-card-gallery.js
+++ src/js/render-card-gallery.js
@@ -1,7 +1,6 @@
 import { getRecipes } from './api';
 import { renderRecipes } from './render-recipe-card';
-import { addHeartsEventListener } from './hearts-listener';
-import { addRecipeButton } from './open-recipe-modal';
+import { addToFavorities } from './hearts-listener';
 
 const cardGallery = document.querySelector('.cards-gallery');
 const allCategoriesBtn = document.querySelector('.all-categories-button');
@@ -10,6 +9,5 @@ export async function renderGallery() {
     const recipes = await getRecipes({});
     cardGallery.innerHTML = await renderRecipes(recipes);
     allCategoriesBtn.classList.add('active-category');
-    addHeartsEventListener();
-    addRecipeButton('.recipe-btn')
+    cardGallery.addEventListener('click', addToFavorities);
 }
