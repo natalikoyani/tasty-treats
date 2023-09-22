1a0ddade774a555ccfb78d07eca9954c0d78d12e
natalikoyani <natalikagami@gmail.com>
2023-09-22 17:03:54 +0200

    final update

--- src/js/theme-dark-mode.js
+++ src/js/theme-dark-mode.js
@@ -1,14 +1,10 @@
-let link = 'theme';
-let styleSheet = findLink(link);
+let link = 'dark-theme.css'
 
 let switcher = document.querySelectorAll('.switcher');
 let localStorageTheme = localStorage.getItem('darkTheme');
 localStorageTheme = localStorageTheme ? JSON.parse(localStorageTheme) : false
 allCheck(localStorageTheme);
-if (styleSheet) {
-     styleSheet.disabled = !localStorageTheme;
-}
-
+setTheme(localStorageTheme);
 
 function allCheck(bol) {
      switcher.forEach(s => s.checked = bol)
@@ -18,15 +14,32 @@ switcher.forEach(s => s.addEventListener('click', (event) => {
      let val = event.target.checked;
      allCheck(val);
      localStorage.setItem('darkTheme', JSON.stringify(val));
-     if (styleSheet) {
-          styleSheet.disabled = !val
-     }
+     setTheme(val);
 }));
 
+function setTheme(val) {
+     if (val) {
+          enableCSS();
+     }
+     else {
+          disableCSS();
+     }
+}
 
+// Включити CSS файл
+function enableCSS() {
+     var cssSheet = document.createElement('link');
+     cssSheet.rel = 'stylesheet';
+     cssSheet.href = `../css/${link}`;
+     document.head.appendChild(cssSheet);
+}
 
-
-function findLink(name) {
-     let links = document.getElementsByTagName('link');
-     return Array(...links).find(link => link.href.indexOf(name) !== -1);
+// Виключити CSS файл
+function disableCSS() {
+     var links = document.getElementsByTagName('link');
+     Array(...links).forEach(l => {
+          if (l.href.indexOf(link) !== -1) {
+               l.parentNode.removeChild(l);
+          }
+     });
 }
