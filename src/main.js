61ae6c7629e31efde3c65bf8bfbb2a2ccca69503
natalikoyani <natalikagami@gmail.com>
2023-09-28 16:10:10 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-// import axios from 'axios';
-// import Swiper from 'swiper';
 import 'swiper/css';
-// import { Notify } from 'notiflix';
-// import SlimSelect from 'slim-select';
 import debounce from 'lodash.debounce';
 
 import './js/hamburger-menu';
